89aaf34a2383a472e6505c51c0b976118fcaad2a
NDERI007 <tnderi373@gmail.com>
2025-06-12 20:41:57 +0300

    hareta dir redo

---
+++ haleta/app/lib/APIreQ.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_BASE = "http://localhost:5000"; // Flask backend
+type FavoritePayload = {
+  prompt: string;
+  favorite: string;
+};
+export const rewritePrompt = async (_prompt: string): Promise<string[]> => {
+  const response = await axios.post(`${API_BASE}/rewrite`, {
+    _prompt,
+  });
+  return response.data.variations; // this is a string[]
+};
+
+export async function logFavorite(data: FavoritePayload) {
+  return axios.post(`${API_BASE}/log_favorite`, data);
+}
